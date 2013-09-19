6126d42a58aafb6e0ddab1cd381ab61bc7507af2
Stian Sandve <stian@sandve.org>
2013-09-19 16:42:55 +0200

    initial commit

---
+++ networking/UDPEchoClient.go
@@ -0,0 +1,36 @@
+/* UDPEchoClient
+ */
+package main
+
+import (
+	"net"
+	"os"
+	"fmt"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.FprintF(os.Stderr, "Usage: echocl host:port\n")
+		os.Exit(1)
+	}
+	service := os.Args[1]
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+
+	_, err = conn.Write([]byte("hello echo"))
+	checkError(err)
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf[0:n]))
+	os.Exit(1)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s ", err.Error())
+		os.Exit(1)
+	}
+}
