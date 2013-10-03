b8279792f4ec71ea698bd62f7f2299b910dba9ba
Stian Sandve <stian@sandve.org>
2013-10-03 14:18:38 +0200

    implemented CSP features

--- lab4/stackcsp.go
+++ lab4/stackcsp.go
@@ -1,20 +1,65 @@
 package lab4
 
-type CspStack struct{}
-
-func NewCspStack() *CspStack {
-	return &CspStack{}
+type CspStack interface {
+	Push(interface{})
+	Pop() (interface{})
+	Len() int
 }
 
-func (cs *CspStack) Len() int {
-	return 0
+func (ss safeStack) Len() int {
+	reply := make(chan interface{})
+	ss <- commandData{action: length, result: reply}
+	return (<-reply).(int)
 }
 
-func (cs *CspStack) Push(value interface{}) {}
-
-func (cs *CspStack) Pop() (value interface{}) {
-	return nil
+func (ss safeStack) Push(element interface{}) {
+	ss <- commandData{action: push, element: element}
 }
 
-func (cs *CspStack) run() {
+func (ss safeStack) Pop() (value interface{}) {
+	reply := make(chan interface{})
+	ss <- commandData{action: pop, result: reply}
+	result := <- reply
+	return result
+}
+
+type safeStack chan commandData
+
+type commandData struct {
+	action commandAction
+	element interface{}
+	result chan<- interface{}
+	data chan<- UnsafeStack
+}
+
+type commandAction int
+
+const (
+	end commandAction = iota
+	pop
+	push
+	length
+)
+
+func NewCspStack() CspStack {
+	ss := make(safeStack)
+	go ss.run()
+	return ss
+}
+
+func (ss safeStack) run() {
+	stack := new(UnsafeStack)
+	for command := range ss {
+		switch command.action {
+		case push:
+			stack.Push(command.element)
+		case pop:
+			command.result <- stack.Pop()
+		case length:
+			command.result <- stack.Len()
+		case end:
+			close(ss)
+			command.data <- *stack
+		}
+	}
 }
