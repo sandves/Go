3cdfa8a3da8bb147ac49a68044de98d5f45e6b85
Stian Sandve <stian@sandve.org>
2013-10-17 10:32:29 +0200

    added new package

---
+++ zaplab/ztorage/ztorage.go
@@ -0,0 +1,29 @@
+package ztorage
+
+import (
+	"github.com/sandves/zaplab/chzap"
+)
+
+type Zaps []chzap.ChZap
+
+func NewZapStore() *Zaps {
+	zs := make(Zaps, 0)
+	return &zs
+}
+
+func (zs *Zaps) StoreZap(z chzap.ChZap) {
+	*zs = append(*zs, z)
+}
+
+func (zs *Zaps) ComputeViewers(chName string) int {
+	viewers := 0
+	for _, v := range *zs {
+		if v.ToChan == chName {
+			viewers++
+		}
+		if v.FromChan == chName {
+			viewers--
+		}
+	}
+	return viewers
+}
