53486de2adfffe0f17e1cc5d649cc201ce1b8811
Stian Sandve <stian@sandve.org>
2013-10-18 14:25:47 +0200

    finished task c, d, and e

--- zaplab/zapserver/zapserver.go
+++ zaplab/zapserver/zapserver.go
@@ -4,41 +4,72 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"strings"
 	"time"
 	"github.com/sandves/zaplab/ztorage"
 	"github.com/sandves/zaplab/chzap"
+	"runtime/pprof"
+	"flag"
 )
 
 var zaps *ztorage.Zaps
 
 func main() {
+	var memprofile = flag.String("memprofile", "", "write memory profile to this file")
+	flag.Parse()
+
 	addr, err := net.ResolveUDPAddr("udp", "224.0.1.130:10000")
 	checkError(err)
 	sock, err := net.ListenMulticastUDP("udp", nil, addr)
 	checkError(err)
 	zaps = ztorage.NewZapStore()
-	go computeViewers("NRK1")
-	go computeViewers("TV2 Norge")
-	go computeZaps()
-	for {
-		 handleClient(sock)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Kill, os.Interrupt)
+
+	go handleClient(sock)
+	go topTenChannels()
+	//go computeViewers("NRK1")
+	//go computeViewers("TV2 Norge")
+	//go computeZaps()
+
+	<-signalChan
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		checkError(err)
+		pprof.WriteHeapProfile(f)
+		f.Close()
+		return
 	}
 }
 
 func handleClient(conn *net.UDPConn) {
-	var buf [1024]byte
-	n, _, err := conn.ReadFromUDP(buf[0:])
-	checkError(err)
-	str := string(buf[:n])
-	strSlice := strings.Split(str, ", ")
-	if len(strSlice) == 5 {
-		var channelZap *chzap.ChZap = chzap.NewChZap(str)
-		zaps.StoreZap(*channelZap)
+	for {
+		var buf [1024]byte
+		n, _, err := conn.ReadFromUDP(buf[0:])
+		checkError(err)
+		str := string(buf[:n])
+		strSlice := strings.Split(str, ", ")
+		if len(strSlice) == 5 {
+			var channelZap *chzap.ChZap = chzap.NewChZap(str)
+			zaps.StoreZap(*channelZap)
+		}
 	}
 }
 
-func computeViewers(chName string) {
+func topTenChannels() {
+	for _ = range time.Tick(1 * time.Second) {
+		fmt.Println()
+		topTen := zaps.TopTenChannels()
+		for i := range topTen {
+			fmt.Printf("Channel %d: %s\n", (i+1),  topTen[i])
+		}
+		fmt.Println()
+	}
+}
+
+/*func computeViewers(chName string) {
 	for _ = range time.Tick(1 * time.Second) {
 		numberOfViewers := zaps.ComputeViewers(chName)
 		fmt.Printf("%s: %d\n", chName, numberOfViewers)
@@ -50,7 +81,7 @@ func computeZaps() {
 		numberOfZaps := len(*zaps)
 		fmt.Printf("Total number of zaps: %d\n", numberOfZaps)
 	}
-}
+}*/
 
 func checkError(err error) {
 	if err != nil {
--- zaplab/ztorage/ztorage.go
+++ zaplab/ztorage/ztorage.go
@@ -4,18 +4,20 @@ import (
 	"github.com/sandves/zaplab/chzap"
 )
 
-type Zaps []chzap.ChZap
+//type Zaps []chzap.ChZap
+type Zaps map[string]int
 
 func NewZapStore() *Zaps {
-	zs := make(Zaps, 0)
+	//zs := make(Zaps, 0)
+	zs := make(Zaps)
 	return &zs
 }
 
-func (zs *Zaps) StoreZap(z chzap.ChZap) {
-	*zs = append(*zs, z)
-}
+/*func (zs *Zaps) StoreZap(z chzap.ChZap) {
+	//*zs = append(*zs, z)
+}*/
 
-func (zs *Zaps) ComputeViewers(chName string) int {
+/*func (zs *Zaps) ComputeViewers(chName string) int {
 	viewers := 0
 	for _, v := range *zs {
 		if v.ToChan == chName {
@@ -26,4 +28,28 @@ func (zs *Zaps) ComputeViewers(chName string) int {
 		}
 	}
 	return viewers
+}*/
+
+func (zs Zaps) StoreZap(z chzap.ChZap) {
+	_, ok := zs[z.ToChan]
+	if !ok {
+		zs[z.ToChan] = 0
+	}
+	_, ok = zs[z.FromChan]
+	if !ok {
+		zs[z.FromChan] = 0
+	}
+
+	for key, _ := range zs {
+		if z.ToChan  == key {
+			zs[key]++
+		}
+		if z.FromChan == key {
+			zs[key]--
+		}
+	}
+}
+
+func (zs Zaps) TopTenChannels() []string {
+	return Top10(zs)
 }
